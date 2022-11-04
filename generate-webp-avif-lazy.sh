3cf3e5a93db143cd38781e6d77a3dc2bfb9a0094
Vincent RABAH <vincent.rabah@gmail.com>
2022-11-04 17:16:16 +0100

    first commit

---
+++ generate-webp-avif-lazy.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+
+# Test if there's how many images to download (must have)
+
+if [ "$#" -ne 1 ];  then
+    echo "You need to specify how many images to download!"
+    exit 
+fi
+
+# Get random images
+
+echo Downloading $1 images
+
+rm -rf origine
+mkdir origine
+for (( i=1; i<=$1; i++ )) 
+do
+#  curl -L https://placeimg.com/900/600 > origine/img$i.jpg
+#  curl -L https://placebeard.it/900/600 > origine/img$i.jpg
+  curl -L https://loremflickr.com/900/600 > origine/img$i.jpg
+done
+
+# Convert/resize images jpg, webp and avif
+
+echo Converting/resizing images
+
+rm -rf img
+mkdir img
+for file in origine/*.jpg 
+do
+  echo -n .
+  basename=$(basename "$file" .jpg)
+  extension="${file##*.}"
+  convert -resize 50% $file img/$basename-l.$extension
+  convert -resize 50% $file img/$basename-l.webp
+  convert -resize 50% $file img/$basename-l.avif
+  convert -resize 25% $file img/$basename-m.$extension
+  convert -resize 25% $file img/$basename-m.webp
+  convert -resize 25% $file img/$basename-m.avif
+  convert -resize 10% $file img/$basename-s.$extension
+  convert -resize 10% $file img/$basename-s.webp
+  convert -resize 10% $file img/$basename-s.avif
+done
+
+# Generate html page
+
+echo
+echo Generate html page
+
+cat << EOF > index.html
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
+        
+        img {
+            width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+EOF
+for (( i=1; i<=$1; i++ ))
+do
+cat << EOF >> index.html
+<picture>
+        <source type="image/avif" media="(max-width: 599px)" srcset="img/img$i-s.avif">
+        <source type="image/avif" media="(min-width: 600px) and (max-width: 1024px)" srcset="img/img$i-m.avif">
+        <source type="image/avif" media="(min-width: 1025px)" srcset="img/img$i-l.avif">
+
+        <source type="image/webp" media="(max-width: 599px)" srcset="img/img$i-s.webp">
+        <source type="image/webp" media="(min-width: 600px) and (max-width: 1024px)" srcset="img/img$i-m.webp">
+        <source type="image/webp" media="(min-width: 1025px)" srcset="img/img$i-l.webp">
+<!--
+        <img src="img/img$i-m.jpg" loading="lazy" decoding="async" width="2032" height="1076" alt="image $1 balise accessible">
+-->
+        <img srcset="img/img$i-s.jpg 150w, img/img$i-m.jpg 375w, img/img$i-l.jpg 750w"
+            sizes="(max-width: 599px) 150px,(max-width: 1025px) 375px, 750px"
+            src="img/img$i-l.jpg" loading="lazy" decoding="async" width="2032" height="1076"
+            alt="image $1 balise accessible">
+
+
+</picture>
+EOF
+done
+cat << EOF >> index.html
+</body>
+</html>
+EOF
+echo open your web browser http://localhost:8080
+python -m SimpleHTTPServer 8080 0.0.0.0 
---
+++ optimisation-img.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+rm -r work-img-optim/*
+mkdir -p work-img-optim/{jpg,gif,png}
+
+# Copie des fichiers dans leur répertoire respectif
+find version-3/ -iname *.jpg | xargs -i cp '{}' work-img-optim/jpg/
+find version-3/ -iname *.png | xargs -i cp '{}' work-img-optim/png/
+find version-3/ -iname *.gif | xargs -i cp '{}' work-img-optim/gif/
+mkdir work-img-optim/tout-en-jpg
+mkdir work-img-optim/tout-en-webp
+mkdir work-img-optim/tout-en-avif
+
+# Copie des fichiers dans le même répertoire et convertion
+  # copie jpg
+find work-img-optim/jpg/ -name *.jpg | xargs -i cp '{}' work-img-optim/tout-en-jpg/
+  # convertion jpg -> webp
+for filename in work-img-optim/jpg/*.jpg; do
+    basename="$(basename "$filename" .jpg)"
+    cwebp -q 84 -af $filename -o work-img-optim/tout-en-webp/$basename.webp
+    avifenc -j all --codec aom --yuv 420 --min 20 --max 25 $filename work-img-optim/tout-en-avif/$basename.avif
+done
+  # convertion png
+for filename in work-img-optim/png/*.png; do
+    basename="$(basename "$filename" .png)"
+    convert "work-img-optim/png/$basename.png" "work-img-optim/tout-en-jpg/$basename.jpg"
+    cwebp -q 84 -af $filename -o work-img-optim/tout-en-webp/$basename.webp
+    avifenc -j all --codec aom --yuv 420 --min 20 --max 25 $filename work-img-optim/tout-en-avif/$basename.avif
+done
+  # convertion gif vers jpg vers webp
+for filename in work-img-optim/gif/*.gif; do
+    basename="$(basename "$filename" .gif)"
+    if [ `identify "$filename" | wc -l` -eq 1 ] ; then
+        convert "work-img-optim/gif/$basename.gif" "work-img-optim/tout-en-jpg/$basename.jpg"
+        cwebp -q 84 -af "work-img-optim/tout-en-jpg/$basename.jpg" -o work-img-optim/tout-en-webp/$basename.webp
+        avifenc -j all --codec aom --yuv 420 --min 20 --max 25 "work-img-optim/tout-en-jpg/$basename.jpg" work-img-optim/tout-en-avif/$basename.avif
+    fi
+done
+
+# Optimisation des jpg à 84% max
+find work-img-optim/tout-en-jpg/ -type f -name '*.jpg' -print0 \
+  | xargs -0n10 -P$(nproc) jpegoptim --max=84 --all-progressive --strip-all
+
+# Affichage gain
+printf "|%4s|%4s|%4s|%50s|\n" "---------" "---------" "---------" "--------------------------------------------------"
+printf "|%4s|%4s|%4s|%50s|\n" "Gain jpeg" "Gain webp" "Gain avif" "Nom"
+printf "|%4s|%4s|%4s|%50s|\n" "---------" "---------" "---------" "--------------------------------------------------"
+for filename in work-img-optim/jpg/*.jpg; do
+  basename="$(basename "$filename" .jpg)"
+  orig=$(stat --format %s $filename)
+  newjpeg=$(stat --format %s work-img-optim/tout-en-jpg/$basename.jpg )
+  newwebp=$(stat --format %s work-img-optim/tout-en-webp/$basename.webp )
+  newavif=$(stat --format %s work-img-optim/tout-en-avif/$basename.avif )
+  gainjpeg=$((100-(newjpeg*100/orig)))
+  gainwebp=$((100-(newwebp*100/orig)))
+  gainavif=$((100-(newavif*100/orig)))
+  printf "|   %4d%% |   %4d%% |   %4d%% |%50s|\n" $gainjpeg $gainwebp $gainavif "$basename.jpg"
+done
+printf "|%4s|%4s|%4s|%50s|\n" "---------" "---------" "---------" "--------------------------------------------------"
+for filename in work-img-optim/png/*.png; do
+  basename="$(basename "$filename" .png)"
+  orig=$(stat --format %s $filename)
+  newjpeg=$(stat --format %s work-img-optim/tout-en-jpg/$basename.jpg )
+  newwebp=$(stat --format %s work-img-optim/tout-en-webp/$basename.webp )
+  gainjpeg=$((100-(newjpeg*100/orig)))
+  gainwebp=$((100-(newwebp*100/orig)))
+  printf "|   %4d%% |   %4d%% |   %4d%% |%50s|\n" $gainjpeg $gainwebp $gainavif "$basename.png"
+done
+printf "|%4s|%4s|%4s|%50s|\n" "---------" "---------" "---------" "--------------------------------------------------"
+for filename in work-img-optim/gif/*.gif; do
+    if [ `identify "$filename" | wc -l` -eq 1 ] ; then
+        basename="$(basename "$filename" .gif)"
+        orig=$(stat --format %s $filename)
+        newjpeg=$(stat --format %s work-img-optim/tout-en-jpg/$basename.jpg )
+        newwebp=$(stat --format %s work-img-optim/tout-en-webp/$basename.webp )
+        gainjpeg=$((100-(newjpeg*100/orig)))
+        gainwebp=$((100-(newwebp*100/orig)))
+        printf "|   %4d%% |   %4d%% |   %4d%% |%50s|\n" $gainjpeg $gainwebp $gainavif "$basename.gif"
+    fi
+done
+printf "|%4s|%4s|%4s|%50s|\n" "---------" "---------" "---------" "--------------------------------------------------"
