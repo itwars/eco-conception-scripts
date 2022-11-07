2a0387351bcfbb7304c637e60d9a5dc21553db7b
Vincent RABAH <vincent.rabah@gmail.com>
2022-11-07 16:57:13 +0100

    ajout install prérequis

---
+++ install-prerequis.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+apk update
+apk upgrade
+apk add imagemagick libwebp-tools libavif-apps vim mc python3
+cat << EOF > /root/.profile
+alias ll='ls -al'
+alias web='python3 -m http.server 80'
+EOF
