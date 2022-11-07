6baa1bbcc42a289e6d2c6af4a7f22af4165d7a3f
Vincent RABAH <vincent.rabah@gmail.com>
2022-11-07 19:53:59 +0100

    fix install

--- install-prerequis.sh
+++ install-prerequis.sh
@@ -2,7 +2,7 @@
 
 apk update
 apk upgrade
-apk add imagemagick libwebp-tools libavif-apps vim mc python3 git wget bash coreutils mailcap
+apk add imagemagick libwebp-tools libavif-apps vim mc python3 git wget bash coreutils mailcap jpegoptim
 cat << EOF > /root/.profile
 alias ll='ls -al'
 alias web='python3 -m http.server 80'
