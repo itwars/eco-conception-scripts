560e2f39c09192e0f718f82f5a1ccda7f2f318a1
Vincent RABAH <vincent.rabah@gmail.com>
2022-11-09 09:24:20 +0100

    ashrc

--- 0-install-prerequis.sh
+++ 0-install-prerequis.sh
@@ -4,8 +4,11 @@ apk update
 apk upgrade
 apk add imagemagick libwebp-tools libavif-apps vim mc python3 git wget bash coreutils mailcap jpegoptim curl npm
 npm i -g svgo
-cat << EOF > /root/.profile
+cat << EOF > /root/.ashrc
 alias ll='ls -al'
 alias web='python3 -m http.server 80'
 export PATH="/root/:$PATH"
+EOF
+cat << EOF > /root/.profile
+source /root/.ashrc
 EOF
