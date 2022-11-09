ea6f95325198372c49f28ebc570f262f40021c0c
Vincent RABAH <vincent.rabah@gmail.com>
2022-11-09 08:53:43 +0100

    add nodejs + svgo

--- 0-install-prerequis.sh
+++ 0-install-prerequis.sh
@@ -2,8 +2,10 @@
 
 apk update
 apk upgrade
-apk add imagemagick libwebp-tools libavif-apps vim mc python3 git wget bash coreutils mailcap jpegoptim curl
+apk add imagemagick libwebp-tools libavif-apps vim mc python3 git wget bash coreutils mailcap jpegoptim curl npm
+npm i -g svgo
 cat << EOF > /root/.profile
 alias ll='ls -al'
 alias web='python3 -m http.server 80'
+export PATH="/root/:$PATH"
 EOF
